e68a3a13f4fb7110bff085cecbaac721cbe02667
Reiner Hüchting <reiner.huechting@gmx.de>
2021-03-26 16:04:31 +0000

    Basis-Implementierung mit leeren Funktionen erstelle, damit alles compiliert.

--- src/de/dhbw/graphen/Graph.java
+++ src/de/dhbw/graphen/Graph.java
@@ -14,7 +14,23 @@ public class Graph implements GraphInterface {
     // [TODO]
     // Muss einen neuen Knoten erzeugen und zur Knotenliste hinzufügen.
   }
+
+  public void addKante(String label1, String label2) {
+    // [TODO]
+    // Muss die beiden Knoten mit den angegebenen Labels suchen und den zweiten beim ersten hinzufügen.
+  }
+
+  public void removeKnoten(String label) {
+    // [TODO]
+    // Muss den angegebenen Knoten aus der Liste der Knoten entfernen.
+  }
+
+  public void removeKante(String label1, String label2) {
+    // [TODO]
+    // Muss den zweiten Knoten aus der Nachbarliste des ersten entfernen.
+  } 
   
+
   // [TODO]
   // ... weitere Funktionen implementieren
 }
